c66a55fe5539ccebdb27c0bae0d1115e235cab66
Burak Can Akıncı <burakakinci.bca@gmail.com>
2022-02-25 22:13:13 +0200

    update

--- app/js/script.js
+++ app/js/script.js
@@ -1,18 +1,23 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.has-fade')
 
 btnHamburger.addEventListener('click', function() {
     console.log('click hamburger');
     
     if(header.classList.contains('open')){ // Close hamburger menu
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElems.forEach (function(element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
     }
-    else { // Open hamburger menu
-        header.classList.add('open');
-        overlay.classList.remove('fade-out');
-        overlay.classList.add('fade-in');
+        else { // Open hamburger menu
+            header.classList.add('open');
+            fadeElems.forEach (function(element) {
+                element.classList.add('fade-in');
+                element.classList.remove('fade-out');
+            });
     }
-})
+});
